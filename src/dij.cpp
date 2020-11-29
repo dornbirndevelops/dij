0344b9e7e97a493b4cc6e2251bd175e6a79d2be8
dornbirndevelops <dornbirndevelops@gmail.com>
2020-11-29 01:00:53 +0100

    code formatter fix

--- src/dij.cpp
+++ src/dij.cpp
@@ -1,6 +1,7 @@
 #include "dij.hpp"
 
-std::unordered_map<std::type_index, std::shared_ptr<void> >& dij::instances() {
-    static std::unordered_map<std::type_index, std::shared_ptr<void> > singleton;
+std::unordered_map<std::type_index, std::shared_ptr<void>> &dij::instances()
+{
+    static std::unordered_map<std::type_index, std::shared_ptr<void>> singleton;
     return singleton;
 }
